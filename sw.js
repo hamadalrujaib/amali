db89378aea83e3ae7ddcc39cec304afa0dd04f7c
hamadalrujaib <rujaib@gmail.com>
2026-09-23 07:10:59 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,8 +1,9 @@
 // عمّالي — Service Worker (تخزين مؤقت للعمل دون اتصال)
-const CACHE = 'amali-v33';
+const CACHE = 'amali-v35';
 const CORE = ['./', './index.html', './manifest.json', './icon-180.png'];
+const FONTS_FILES = ['plex','naskh','cairo','tajawal','almarai','amiri','kufi','readex','markazi'].flatMap(f=>['./fonts/'+f+'-400.woff2','./fonts/'+f+'-700.woff2']).concat(['./fonts/plex-600.woff2','./fonts/kufi-600.woff2','./fonts/readex-600.woff2']);
 const OCR = ['./ocr/tesseract.min.js', './ocr/worker.min.js', './ocr/tesseract-core-simd-lstm.wasm.js', './ocr/tesseract-core-lstm.wasm.js', './ocr/ara.wasm'];
-self.addEventListener('install', e => { self.skipWaiting(); e.waitUntil(caches.open(CACHE).then(c => c.addAll(CORE).catch(() => {}))); });
+self.addEventListener('install', e => { self.skipWaiting(); e.waitUntil(caches.open(CACHE).then(c => c.addAll(CORE.concat(FONTS_FILES)).catch(() => {}))); });
 self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim())); });
 self.addEventListener('fetch', e => {
   const url = new URL(e.request.url);
